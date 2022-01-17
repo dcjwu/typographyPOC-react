68aaa0acced26d7f97d6134ac7de6aa274913573
AK <kkucenko.aleksejs@gmail.com>
2022-01-17 18:43:06 +0200

    Prohibited non-auth user to view particular product page.

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -9,6 +9,7 @@ const ProductPage = () => {
    const {id} = useParams()
    const dispatch = useDispatch()
    const {productById, isLoaded} = useSelector(({products}) => products)
+   const {currentUser} = useSelector(({auth}) => auth)
 
    useEffect(() => {
       dispatch(getProductById(+id))
@@ -18,23 +19,29 @@ const ProductPage = () => {
    }, [id])
 
    return (
-      <div className="product">
+      <>
          {
-            !isLoaded
-               ? <Spinner/>
-               : productById &&
-               <div className="container-content">
-                  <div className="product-header">
-                     <div className="product-text">
-                        <h2>{productById.title}</h2>
-                        <p><small className="mt-4">Description...</small></p>
-                     </div>
-                     <img src={productById.imageUrl} alt="Product"/>
-                  </div>
-                  <Calculator calculatorType={productById.calculator}/>
+            !currentUser
+               ? <h1 className='alert-danger text-center p-5'>Please, Sign In to view this Page!</h1>
+               : <div className="product">
+                  {
+                     !isLoaded
+                        ? <Spinner/>
+                        : productById &&
+                        <div className="container-content">
+                           <div className="product-header">
+                              <div className="product-text">
+                                 <h2>{productById.title}</h2>
+                                 <p><small className="mt-4">Description...</small></p>
+                              </div>
+                              <img src={productById.imageUrl} alt="Product"/>
+                           </div>
+                           <Calculator calculatorType={productById.calculator}/>
+                        </div>
+                  }
                </div>
          }
-      </div>
+         </>
    )
 }
 
